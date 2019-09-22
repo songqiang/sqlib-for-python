d7b9c5706987e6eeecb50300ec86f83c449a3227
Song, Qiang <keeyang@ustc.edu>
2019-09-22 00:25:57 -0700

    add ipython_magics moduel
    provide timestamp cell magic

---
+++ ipython_magics.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+""" Jupyter magics for ipython
+Song, Qiang <keeyang@ustc.edu> 2019
+"""
+
+from IPython.core import magic_arguments
+from IPython.core.magic import (line_magic,
+                                cell_magic,
+                                line_cell_magic,
+                                Magics,
+                                magics_class)
+
+@magics_class
+class TimeStamp(Magics):
+    @staticmethod
+    def strftime_timedelta(td, fmt = '%H:%M:%S.%f'):
+        days = td.days
+        hours, rem = divmod(td.seconds, 3600)
+        minutes, rem = divmod(rem, 60)
+        seconds = rem
+
+        fmt = (fmt.replace('%D', '{days:d}')
+                  .replace('%H', '{hours:02d}')
+                  .replace('%M', '{minutes:02d}')
+                  .replace('%S', '{seconds:02d}')
+                  .replace('%f', '{milli:03.0f}')
+                )
+        return fmt.format(days = days,
+                          hours = hours,
+                          minutes = minutes,
+                          seconds = seconds,
+                          milli = td.microseconds / 1000)
+
+    @cell_magic
+    def timestamp(self, line, cell):
+        from datetime import datetime, timedelta
+        start = datetime.now()
+        self.shell.run_cell(cell)
+        end = datetime.now()
+        duration = end - start
+        print(f'start {start:%Y-%m-%d %H:%M:%S}, finish {end:%Y-%m-%d %H:%M:%S}, duration {self.strftime_timedelta(duration)}')
+
+    @cell_magic
+    def exectime(self, *args):
+        self.timestamp(*args)
+
+    @cell_magic
+    def ts(self, *args):
+        self.timestamp(*args)
+
+def register_magics(*magics):
+    ip = get_ipython()
+    for m in magics:
+        ip.register_magics(m)
+
+register_magics(TimeStamp)
