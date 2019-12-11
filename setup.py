1979b3c8276577b8ed22910dceb0851d0ae58014
Song, Qiang <keeyang@ustc.edu>
2019-12-11 01:15:11 -0800

    re-org into standard python package

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name="sqpylib",
+    version=0.1,
+    description="utility modules for python",
+    author="Song, Qiang",
+    maintainer="Song, Qiang",
+    maintainer_email="keeyang@ustc.edu",
+    license="GPLv3",
+    keywords="python pandas jupyter",
+    packages=["sqpylib"],
+    zip_safe=True,
+)
